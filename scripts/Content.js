2ba938ad715e7cb92f15117f721bdc26320bd59a
Ubuntu <ubuntu@ip-172-31-18-251.ec2.internal>
2019-09-09 12:55:19 +0000

    adding scripts folder

---
+++ scripts/Content.js
@@ -0,0 +1,8 @@
+    
+import * as React from 'react';
+
+export class Content extends React.Component {
+    render() {
+        return <div><h1>Hello World from React!</h1></div>;
+    }
+}
---
+++ scripts/Main.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { Content } from './Content';
+
+ReactDOM.render(<Content />, document.getElementById('content'));
