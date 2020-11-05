6a44433160189656b4b2cb9ae9fa3eef1c3984b7
Jay Amin <aminjay867@gmail.com>
2020-11-05 17:51:20 -0500

    User can get definition of the term he is looking for

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import os
 import flask
 import flask_socketio
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
 
 app = flask.Flask(__name__)
 socketio = flask_socketio.SocketIO(app)
@@ -12,7 +18,15 @@ def hello():
 
 @socketio.on('send message')
 def send_message(text):
-    messageReceived = text
+    response = requests.get(f'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{text}?key={API_KEY}')
+    result = response.json()
+    messageReceived = ''
+    try:
+        definition = result[0]['shortdef']
+        messageReceived = ', '.join(definition)
+    except:
+        messageReceived = "Sorry, we can't find the definition of the term you are looking for."
+
     socketio.emit('forward message', messageReceived)
 
 if __name__ == '__main__':
