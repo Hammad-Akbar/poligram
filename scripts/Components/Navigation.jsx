641088a94bc6c3c57e2f8263d6a8e9812488620c
Akhil Samarth <akhilsamarth236@gmail.com>
2020-11-07 22:02:12 +0000

    Added link to quiz on homepage

--- scripts/Components/Navigation.jsx
+++ scripts/Components/Navigation.jsx
@@ -4,10 +4,11 @@ import { NavLink } from 'react-router-dom';
  
 const Navigation = () => {
     return (
-       <div>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/Dictionary">Dictionary</NavLink>
-       </div>
+        <div>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/Dictionary">Dictionary</NavLink>
+            <NavLink to="/Quiz">Quiz</NavLink>
+        </div>
     );
 }
  
