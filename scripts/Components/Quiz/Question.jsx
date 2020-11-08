e17b28c8557a97a0c0493a84f28630dd86f156eb
Akhil Samarth <akhilsamarth236@gmail.com>
2020-11-08 17:07:14 +0000

    Added labels and divs to question blocks

--- scripts/Components/Quiz/Question.jsx
+++ scripts/Components/Quiz/Question.jsx
@@ -4,12 +4,30 @@ function Question() {
     
     return (
         <div>
-            <input type="radio" name="quiz_answer" value="d3" />
-            <input type="radio" name="quiz_answer" value="d2" />
-            <input type="radio" name="quiz_answer" value="d1" />
-            <input type="radio" name="quiz_answer" value="a1" />
-            <input type="radio" name="quiz_answer" value="a2" />
-            <input type="radio" name="quiz_answer" value="a3" />
+            <div>
+                <input type="radio" name="quiz_answer" id="d3" value="d3" /><br />
+                <label for="d3">Strongly Disagree</label>
+            </div>
+            <div>
+                <input type="radio" name="quiz_answer" id="d2" value="d2" /><br />
+                <label for="d2">Disagree</label>
+            </div>
+            <div>
+                <input type="radio" name="quiz_answer" id="d1" value="d1" /><br />
+                <label for="d1">Somewhat Disagree</label>
+            </div>
+            <div>
+                <input type="radio" name="quiz_answer" id="a1" value="a1" /><br />
+                <label for="a1">Somewhat Agree</label>
+            </div>
+            <div>
+                <input type="radio" name="quiz_answer" id="a2" value="a2" /><br />
+                <label for="a2">Agree</label>
+            </div>
+            <div>
+                <input type="radio" name="quiz_answer" id="a3" value="a3" /><br />
+                <label for="a3">Strongly Agree</label>
+            </div>
         </div>
     );
 }
