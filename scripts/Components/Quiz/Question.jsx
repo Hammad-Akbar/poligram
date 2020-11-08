b7b4a4a10710c61b9ba580fbeaa443c724599b13
Akhil Samarth <akhilsamarth236@gmail.com>
2020-11-08 17:00:18 +0000

    Added radio buttons for answers

--- scripts/Components/Quiz/Question.jsx
+++ scripts/Components/Quiz/Question.jsx
@@ -4,7 +4,12 @@ function Question() {
     
     return (
         <div>
-            <h3>This is a question</h3>
+            <input type="radio" name="quiz_answer" value="d3" />
+            <input type="radio" name="quiz_answer" value="d2" />
+            <input type="radio" name="quiz_answer" value="d1" />
+            <input type="radio" name="quiz_answer" value="a1" />
+            <input type="radio" name="quiz_answer" value="a2" />
+            <input type="radio" name="quiz_answer" value="a3" />
         </div>
     );
 }
