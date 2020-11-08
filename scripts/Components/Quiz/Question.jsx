d9a410d98cca3eed5b7e2f786cdc33c9ca42e89f
Akhil Samarth <akhilsamarth236@gmail.com>
2020-11-08 16:53:10 +0000

    Added question component

---
+++ scripts/Components/Quiz/Question.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Question() {
+    
+    return (
+        <div>
+            <h3>This is a question</h3>
+        </div>
+    );
+}
+
+export default Question;
--- scripts/Components/Quiz/Quiz.jsx
+++ scripts/Components/Quiz/Quiz.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
+import Question from "./Question";
+
 function Quiz() {
     
     return (
         <div>
             <h2>This is the quiz</h2>
+            <Question />
         </div>
     );
 }
