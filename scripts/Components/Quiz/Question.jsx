780c129d3327f3b37fb051c6f99352f09ea06362
Akhil Samarth <akhilsamarth236@gmail.com>
2020-11-08 17:18:22 +0000

    Added basic styling to get radio buttons inline with centered labels

--- scripts/Components/Quiz/Question.jsx
+++ scripts/Components/Quiz/Question.jsx
@@ -2,29 +2,35 @@ import React from "react";
 
 function Question() {
     
+    const answerBlockStyle = {
+        display: "inline-block",
+        paddingRight: "2em",
+        textAlign: "center"
+    };
+    
     return (
         <div>
-            <div>
+            <div style={answerBlockStyle}>
                 <input type="radio" name="quiz_answer" id="d3" value="d3" /><br />
                 <label for="d3">Strongly Disagree</label>
             </div>
-            <div>
+            <div style={answerBlockStyle}>
                 <input type="radio" name="quiz_answer" id="d2" value="d2" /><br />
                 <label for="d2">Disagree</label>
             </div>
-            <div>
+            <div style={answerBlockStyle}>
                 <input type="radio" name="quiz_answer" id="d1" value="d1" /><br />
                 <label for="d1">Somewhat Disagree</label>
             </div>
-            <div>
+            <div style={answerBlockStyle}>
                 <input type="radio" name="quiz_answer" id="a1" value="a1" /><br />
                 <label for="a1">Somewhat Agree</label>
             </div>
-            <div>
+            <div style={answerBlockStyle}>
                 <input type="radio" name="quiz_answer" id="a2" value="a2" /><br />
                 <label for="a2">Agree</label>
             </div>
-            <div>
+            <div style={answerBlockStyle}>
                 <input type="radio" name="quiz_answer" id="a3" value="a3" /><br />
                 <label for="a3">Strongly Agree</label>
             </div>
