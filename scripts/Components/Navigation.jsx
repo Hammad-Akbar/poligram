78d7ef5b9382c1742f6b47942e36e27fc9803eaa
EC2 Default User <svp62@njit.edu>
2020-11-10 15:30:53 +0000

    fixing merge conflicts

--- scripts/Components/Navigation.jsx
+++ scripts/Components/Navigation.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import './styles/home.css'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './styles/home.css';
  
-const Navigation = () => {
+const Navigation = ({user}) => {
     return (
-       <div className='topnav'>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/Dictionary">Dictionary</NavLink>
-          <NavLink to="/News">News</NavLink>
-       </div>
+        <div className='topnav'>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/Dictionary">Dictionary</NavLink>
+            <NavLink to="/Quiz">Quiz</NavLink>
+            <NavLink to="/News">News</NavLink>
+            <p>Welcome: {user}</p>
+        </div>
     );
-}
+};
  
 export default Navigation;
