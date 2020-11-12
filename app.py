da69e570c18d338f4e9451e1ce7fb4e1395fe6e6
Jay Amin <aminjay867@gmail.com>
2020-11-12 13:55:43 -0500

    Completed testing for models.py and new user

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-import flask 
+import flask
 from flask import request
 import flask_socketio
 import flask_sqlalchemy
@@ -7,7 +7,6 @@ import requests
 import dotenv
 import json
 import random
-
 
 dotenv.load_dotenv()
 
@@ -26,9 +25,20 @@ db.init_app(app)
 db.app = app
 
 
+def messageDict(result):
+    messageReceived = ''
+    if 'shortdef' in result[0]:
+        definition = result[0]['shortdef']
+        messageReceived = ', '.join(definition)
+    else:
+        messageReceived = "Sorry, we can't find the definition of the term you are looking for."
+    return messageReceived
+
+
 @app.route('/')
 def hello():
     return flask.render_template('index.html')
+
 
 @socketio.on('connect user')
 def on_connect(userProfile):
@@ -43,6 +53,7 @@ def on_connect(userProfile):
         "userImage": image
     }, room=socketId)
 
+
 @socketio.on('send message')
 def send_message(text):
     socketId = request.sid
@@ -50,13 +61,7 @@ def send_message(text):
         f'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{text}?key={DICTIONARY_API_KEY}')
     result = response.json()
 
-    messageReceived = ''
-    if 'shortdef' in result[0]:
-        definition = result[0]['shortdef']
-        messageReceived = ', '.join(definition)
-    else:
-        messageReceived = "Sorry, we can't find the definition of the term you are looking for."
-
+    messageReceived = messageDict(result)
     socketio.emit('forward message', {
         'messageReceived': messageReceived
     }, room=socketId)
@@ -67,62 +72,62 @@ def request_quiz():
     questions_file = open('quiz_questions.json', 'r')
     questions_json = json.load(questions_file)
     questions_file.close()
-    
+
     groups = questions_json['groups']
     group_indexes = [n for n in range(len(groups))]
     random.shuffle(group_indexes)
-    
+
     quiz_out = []
-    
+
     for i in group_indexes:
         questions = groups[i]['questions']
         q_index = random.randrange(len(questions))
-        
+
         quiz_out.append(questions[q_index])
-        
+
     sid = flask.request.sid
-    
+
     socketio.emit('quiz generated', quiz_out, room=sid)
-    
+
 
 @socketio.on('news api call')
 def news_api_call():
-	print("Got an event for newz:")
-	url = 'https://newsapi.org/v2/everything'
-	parameters = {
-	    'q': 'politics', # query phrase
-	    'pageSize': 15,# maximum is 100
-	    'apiKey': NEWS_API_KEY
-	}
+    print("Got an event for newz:")
+    url = 'https://newsapi.org/v2/everything'
+    parameters = {
+        'q': 'politics',  # query phrase
+        'pageSize': 15,  # maximum is 100
+        'apiKey': NEWS_API_KEY
+    }
 
-	response = requests.get(url, params=parameters)
-	response_json = response.json()
-	news_list = response_json["articles"]
-	
-	newsObjectLst = []
+    response = requests.get(url, params=parameters)
+    response_json = response.json()
+    news_list = response_json["articles"]
 
-	for news in news_list:
-		news_content = news["content"].split("…")
-		final_news_content = str(news_content[0]) + "(continue reading)... "
-		newsObjectLst.append(
-			{
-			'title': news["title"], 
-			'author': news["author"], 
-			'content': final_news_content, 
-			'published': news["publishedAt"], 
-			'source': news["source"]["name"], 
-			'url': news["url"], 
-			'img': news["urlToImage"]
-			}
-		)
+    newsObjectLst = []
 
-	socketio.emit('newsData', {
-		'newsObjectLst': newsObjectLst
-	})
+    for news in news_list:
+        news_content = news["content"].split("…")
+        final_news_content = str(news_content[0]) + "(continue reading)... "
+        newsObjectLst.append(
+            {
+                'title': news["title"],
+                'author': news["author"],
+                'content': final_news_content,
+                'published': news["publishedAt"],
+                'source': news["source"]["name"],
+                'url': news["url"],
+                'img': news["urlToImage"]
+            }
+        )
 
-    
+    socketio.emit('newsData', {
+        'newsObjectLst': newsObjectLst
+    })
+
+
 if __name__ == '__main__':
-	socketio.run(
+    socketio.run(
         app,
         host=os.getenv('IP', '0.0.0.0'),
         port=int(os.getenv('PORT', 8080)),
--- tests/mocked_unit_tests.py
+++ tests/mocked_unit_tests.py
@@ -4,11 +4,48 @@ from os.path import dirname, join
 from unittest.mock import patch
 
 sys.path.insert(1, join(dirname(__file__), '../'))
+import app
 
 
 class mockedTest(unittest.TestCase):
-    def test_dictionary(self):
-        print('true')
+    @patch("app.send_message")
+    def test_dictionary_failure(self, mocked_get):
+        mocked_get.return_value.json.return_value = ["added", "daddy", "add", "adds", "dad", "dads", "dead",
+                                                     "deed", "did", "dido",
+                                                     "died", "dodo", "dud", "dude", "duds", "dyad",
+                                                     "dyed", "eddy", "odd", "odds"]
+
+        response = app.messageDict(mocked_get.return_value.json.return_value)
+        result = "Sorry, we can't find the definition of the term you are looking for."
+        self.assertEqual(response, result)
+
+    @patch("app.send_message")
+    def test_dictionary_success(self, mocked_get):
+        mocked_get.return_value.json.return_value = [{"shortdef": ["a piece of paper indicating a person\u0027s "
+                                                                   "preferences in an election",
+                                                                   "the right to formally express one\u0027s "
+                                                                   "position "
+                                                                   "or will in an election"]}]
+        response = app.messageDict(mocked_get.return_value.json.return_value)
+        result = "a piece of paper indicating a person\u0027s " \
+                 "preferences in an election, " \
+                 "the right to formally express one\u0027s "\
+                 "position "\
+                 "or will in an election"
+        self.assertEqual(response, result)
+
+    def test_new_user_connection(self):
+        flask_test_client = app.app.test_client()
+        socketio_test_client = app.socketio.test_client(app.app,
+                                                        flask_test_client=flask_test_client)
+        socketio_test_client.emit('connect user', {
+            "name": "Jay Amin",
+            "email": "juiamin1000@gmail.com",
+            "imageUrl": "profile.jpg"
+        })
+        result = socketio_test_client.get_received()
+        user = result[0]['args'][0]['user']
+        self.assertEqual(user, 'Jay Amin')
 
 
 if __name__ == '__main__':
