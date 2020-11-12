1e0356e17c54b2cf3aeedfcbe5d9f882e41cd89d
Jay Amin <aminjay867@gmail.com>
2020-11-11 21:28:34 -0500

    Started adding tests

---
+++ tests/unmocked_unit_tests.py
@@ -0,0 +1,15 @@
+import unittest
+import sys
+from os.path import dirname, join
+from unittest.mock import patch
+
+sys.path.insert(1, join(dirname(__file__), '../'))
+
+class unmockedTest(unittest.TestCase):
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
