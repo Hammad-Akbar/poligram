fa84c3aa9e11fbb6109399deab7f51b324eaa890
EC2 Default User <svp62@njit.edu>
2020-11-12 19:13:45 +0000

    unmocked test for news api

--- tests/unmocked_test.py
+++ tests/unmocked_test.py
@@ -15,7 +15,12 @@ class TestNews(unittest.TestCase):
 	    
 	    for i in r_json:
 	        self.assertFalse(i["source"] == " ", "False or True")
-	        
+	        self.assertFalse(i["author"] == " ", "False or True")
+	        self.assertFalse(i["title"] == " ", "False or True")
+	        self.assertFalse(i["img"] == " ", "False or True")
+	        self.assertFalse(i["url"] == " ", "False or True")
+	        self.assertFalse(i["published"] == " ", "False or True")
+	        self.assertFalse(i["content"] == " ", "False or True")
 	        
 
 if __name__ == "__main__":
