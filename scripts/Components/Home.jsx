e9e0ff3278d1e420121040d8fd0fa0e7e54216f6
EC2 Default User <haa38@njit.edu>
2020-11-23 18:07:23 +0000

    Fix text overlapping into other sections

--- scripts/Components/Home.jsx
+++ scripts/Components/Home.jsx
@@ -21,27 +21,39 @@ export default function Home() {
 
   return (
     <body>
-      <div className="rectangle-red">
+      <section id="section-red">
         <div className="link-bar">
           <div className="heading"> Poligram </div>
-          <div className="slogan"> The politicians handbook for everyone. </div>
-          <div className="slogan-description"> Poligram will provide an interactive way to learn politics and politicians in America.  </div>
+          <div className="slogan-white"> The politicians handbook for everyone. </div>
+          <div className="slogan-description-white"> Poligram will provide an interactive way to learn politics and politicians in America.  </div>
         </div>
-      </div>
-      <div className="horizontal-line" />
-      <div className="rectangle-white">
-        <div className="slogan-black"> Features </div>
+      </section>
+      <div className="horizontal-line"/>
+      <section id="section-white">
+        <div className="slogan-black"> Features </div> 
         <div className="slogan-description-black"> Poligram will feature a way to search for politicians and learn about their views on topics, take meaningful quizzes to test your knowledge, and keep up with current issues in the news.  </div>
-        <div className="section-1"> News </div>
-        <div className="section-1-description"> Keep up with news related to politics and all your followed politicians. </div>
-      </div>
-      <div className="horizontal-line" />
-      <div className="rectangle-blue">
-        <div className="slogan">
+        <div className="box">
+          <div> 
+            <div className="section-heading"> News </div>
+            <div className="section-description"> Keep up with news related to politics and all your followed politicians! </div>
+          </div>
+          <div>
+            <div className="section-heading"> Dictionary </div>
+            <div className="section-description"> Search up political words that you do not understand! </div>
+          </div>
+          <div>
+            <div className="section-heading"> Quiz </div>
+            <div className="section-description"> Take a quiz and discover your idealogy! </div>
+          </div>
+        </div>
+      </section>
+      <div className="horizontal-line"/>
+      <section id="section-blue">
+        <div className="slogan-white">
           <span className="message">Get in touch with us!</span>
         </div>
         <FeedbackButton />
-      </div>
+      </section>
     </body>
   );
 }
