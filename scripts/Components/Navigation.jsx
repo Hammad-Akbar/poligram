6fb5b8c2c22d5cd7d95aa1e6448bf872c252b63a
EC2 Default User <haa38@njit.edu>
2020-11-24 18:19:27 +0000

    Add HashRouter to fix reload issues

--- scripts/Components/Navigation.jsx
+++ scripts/Components/Navigation.jsx
@@ -1,12 +1,5 @@
 import React, {useState} from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch,
-  Redirect,
-  useLocation
-} from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 import LoginPage from './LoginPage'
 import './styles/home.css';
 
@@ -17,74 +10,38 @@ const Navigation = ({ user }) => {
   function Authenticated() {
     if (isAuth) {
       return (
-        <Router>
+        <>
           <div className="topnav">
-            <Link to="/">Home</Link>
-            <Link to="/Dictionary">Dictionary</Link>
-            <Link to="/Quiz">Quiz</Link>
-            <Link to="/News">News</Link>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/Dictionary">Dictionary</NavLink>
+            <NavLink to="/Quiz">Quiz</NavLink>
+            <NavLink to="/News">News</NavLink>
             <p>
-              Welcome: 
+              Welcome:
               {user}
             </p>
           </div>
-          <Switch>
-          
-            <Route exact path="/">
-              <Home />
-            </Route>
-            
-            <Route path="/Dictionary">
-              <Redirect to="/Dictionary" />
-            </Route>
-            
-            <Route path="/Quiz">
-              <Redirect to="/Quiz" />
-            </Route>
-            
-            <Route path="/News">
-              <Redirect to="/News" />
-            </Route>
-            
-          </Switch>
-        </Router>
+        </>
       )
     }
     return (
-      <Router>
-        <div className="topnav">
-          <Link to="/">Home</Link>
-          <Link to="/Dictionary">Dictionary</Link>
-          <Link to="/Quiz">Quiz</Link>
-          <Link to="/News">News</Link>
-        </div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          
-          <Route path="/Dictionary">
-            <Redirect to="/Dictionary" />
-          </Route>
-          
-          <Route path="/Quiz">
-            <Redirect to="/Quiz" />
-          </Route>
-          
-          <Route path="/News">
-            <Redirect to="/News" />
-          </Route>
-          
-        </Switch>
-      </Router>
-    )
+      <>
+        <>
+          <div className="topnav">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/Dictionary">Dictionary</NavLink>
+            <NavLink to="/Quiz">Quiz</NavLink>
+            <NavLink to="/News">News</NavLink>
+            <LoginPage setIsAuth={setIsAuth} />
+          </div>
+        </>
+      </>
+    );
   }
-
   return(
     <div>
       {Authenticated()}
     </div>
   )
 };
-
 export default Navigation;
