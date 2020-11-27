e9e25ba82fdf16cf03fbcb025a039f6c2e318e9b
Jay Amin <aminjay867@gmail.com>
2020-11-27 16:18:46 -0500

    Added USA map and event listener

--- scripts/Components/Map.jsx
+++ scripts/Components/Map.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import USAMap from 'react-usa-map';
 import './styles/map.css';
 
 function Map() {
+  function mapHandler(event) {
+    alert(event.target.dataset.name);
+  }
+
+  function statesCustomConfig() {
+    return {
+      NJ: {
+        fill: 'navy',
+      },
+      NY: {
+        fill: '#CC0000',
+      },
+    };
+  }
+
   return (
     <div className="map">
-      USA MAP
+      <USAMap onClick={mapHandler} customize={statesCustomConfig()} />
     </div>
   );
 }
