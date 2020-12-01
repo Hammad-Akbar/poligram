60d22dc009631f6bb77f37456903c7c55b029d5b
EC2 Default User <svp62@njit.edu>
2020-12-01 20:38:14 +0000

    Get data for trending news

--- scripts/Components/News.jsx
+++ scripts/Components/News.jsx
@@ -5,21 +5,61 @@ import './styles/news.css';
 
 function News() {
   const [newsData, setNewsData] = useState([]);
+  const [trendNews, setTrendNews] = useState([]);
   useEffect(() => {
     Socket.emit('news api call');
-
+    
     Socket.on('newsData', (data) => {
       setNewsData(data.newsObjectLst);
+    });
+    Socket.on('trendNews', (data) => {
+      setTrendNews(data.TrendnewsLst);
     });
 
     return () => {
       Socket.off('newsData');
+      Socket.off('trendNews');
     };
   }, []);
+  
+  console.log(trendNews);
 
   return (
     <div> 
     <NewsSearch /> 
+    <div>
+    {trendNews.map((newz) => (
+        <p>
+          
+          <div className="headerclass">{newz.title}</div>
+          <br />
+
+          <div className="row">
+            <div className="column">
+              <p>
+                Article:
+                {newz.content}
+                <a href={newz.url} target="_blank" rel="noopener noreferrer">Click Here</a>
+              </p>
+              <p>
+                Written by:
+                {newz.author}
+              </p>
+              <p>
+                Posted by:
+                {newz.source}
+              </p>
+            </div>
+
+            <div className="column"><img src={newz.img} alt="" width="300" height="300" /></div>
+
+          </div>
+          <br />
+
+        </p>
+
+      ))}
+      </div>
       {newsData.map((news) => (
         <p>
           
