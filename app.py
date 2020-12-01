790f7e6c173c3a75057f1741dce5a13b85f780fa
EC2 Default User <svp62@njit.edu>
2020-12-01 20:37:33 +0000

    created method for trending news api call

--- app.py
+++ app.py
@@ -177,20 +177,62 @@ def api_call_for_news(data):
     socketio.emit('newsData', {
         'newsObjectLst': newsObjectLst
     })
+    
+    
+    
 
     return response_json["articles"]
 
+def trending_news():
+    "Trending news Api call"
+    trend_url = 'https://newsapi.org/v2/top-headlines'
+    parameter = {
+        'country': 'us',
+        'q': 'trump', # query phrase
+        'pageSize': 3,  # maximum is 100
+        'apiKey': NEWS_API_KEY
+    }
+
+    response_trend = requests.get(trend_url, params=parameter)
+    Response_json = response_trend.json()
+    print("Trending \n", Response_json)
+    
+    return Response_json["articles"]
 
 @socketio.on('news api call')
 def news_api_call():
     """ sending news back to client """
-    print("Got an event for newz:")
     news_list = api_call_for_news('politics')
-
+    
     socketio.emit('newsData', {
         'newsObjectLst': newsObjectLst
     })
+    
+    news_Trend = trending_news()
+    
+    TrendnewsLst = []
+    for newz in news_Trend:
+        if newz["content"] == None:
+            final_news_content = "To read full article... "
+        else:
+            news_content = newz["content"].split("…")
+            final_news_content = str(news_content[0]) + "(continue reading)... "
 
+        TrendnewsLst.append(
+            {
+                'title': newz["title"],
+                'author': newz["author"],
+                'content': final_news_content,
+                'published': newz["publishedAt"],
+                'source': newz["source"]["name"],
+                'url': newz["url"],
+                'img': newz["urlToImage"]
+            }
+        )
+    socketio.emit('trendNews', {
+        'TrendnewsLst': TrendnewsLst
+    })
+    
     return newsObjectLst
 
 
