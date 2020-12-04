6a5d3110f22bf2d65890cfaf20ddee399bf662e9
EC2 Default User <svp62@njit.edu>
2020-12-04 07:59:10 +0000

    added words for query

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ def api_call_for_news(data):
 
 def trending_news():
     "Trending news Api call"
-    query = ['virus']
+    query = ['Trump', 'Biden', 'election', 'obama', 'Republican', 'democrat', 'governor', 'politics', 'government', 'law', 'state', 'union', 'bills', 'congress']
     random_query=(random.choices(query))
     print(random_query)
     trend_url = 'https://newsapi.org/v2/top-headlines'
@@ -195,7 +195,6 @@ def trending_news():
 
     response_trend = requests.get(trend_url, params=parameter)
     Response_json = response_trend.json()
-    print(Response_json["articles"])
     return Response_json["articles"]
 
 @socketio.on('news api call')
