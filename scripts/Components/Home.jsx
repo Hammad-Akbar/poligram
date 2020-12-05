b8b5580fcc83d6d4be950d73c9d90f602a507533
EC2 Default User <haa38@njit.edu>
2020-12-05 22:51:42 +0000

    Created image slider

--- scripts/Components/Home.jsx
+++ scripts/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FeedbackButton from './FeedbackButton';
+import Carousel from './InfoCards.jsx';
 import Socket from './Socket';
 import './styles/home.css';
 
@@ -25,13 +26,15 @@ export default function Home() {
         <div className="link-bar">
           <div className="heading"> Poligram </div>
           <div className="slogan-white"> The politicians handbook for everyone. </div>
-          <div className="slogan-description-white"> Poligram will provide an interactive way to learn politics and politicians in America.  </div>
+          <div className="slogan-description-white"> 
+            With politics more important than ever, Poligram makes it easy to learn. Poligram will provide an interactive way to learn about politics in America. 
+          </div>
         </div>
       </section>
       <div className="horizontal-line"/>
       <section id="section-white">
         <div className="slogan-black"> Features </div> 
-        <div className="slogan-description-black"> Poligram will feature a way to search for politicians and learn about their views on topics, take meaningful quizzes to test your knowledge, and keep up with current issues in the news.  </div>
+        <div className="slogan-description-black"> Poligram will feature a way to learn about new political words, take meaningful quizzes to test your knowledge, and keep up with current issues in the news.  </div>
         <div className="box">
           <div> 
             <div className="section-heading"> News </div>
@@ -43,12 +46,23 @@ export default function Home() {
           </div>
           <div>
             <div className="section-heading"> Quiz </div>
-            <div className="section-description"> Take a quiz and discover your idealogy! </div>
+            <div className="section-description"> Take a quiz and discover your political idealogy! </div>
           </div>
         </div>
       </section>
       <div className="horizontal-line"/>
       <section id="section-blue">
+        <div className="slogan-white">
+          <span className="message">Meet the team!</span>
+        </div>
+        <div className="slogan-description-white2"> We are a group of people who want to show an emphasis on politics. </div>
+        <Carousel />
+        <br></br>
+        <br></br>
+        <br></br>
+      </section>
+      <div className="horizontal-line"/>
+      <section id="section-red">
         <div className="slogan-white">
           <span className="message">Get in touch with us!</span>
         </div>
