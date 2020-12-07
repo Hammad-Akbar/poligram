7c07ab45cfff10274267b1382ef79bbc59ec36cb
EC2 Default User <svp62@njit.edu>
2020-12-07 21:32:53 +0000

    removed extra whitespaces and added string line for all methods

--- tests/unmocked_unit_tests.py
+++ tests/unmocked_unit_tests.py
@@ -1,5 +1,6 @@
 """Unmocked unit test"""
 #pylint: disable=C0103
+#pylint: disable=C0301
 import unittest
 from os.path import dirname, join
 import sys
@@ -16,8 +17,7 @@ class UnmockedTest(unittest.TestCase):
         self.quiz_test_question = models.QuizQuestions('Test question for unit test', 'test group', -32)
         self.test_user = models.UserInfo("test@test.com", "Test McTestface", "http://this-is-a-fake-url.com")
         self.test_quiz_score = models.QuizScore("test@test.com", -99)
-        
-        
+
     def test_app_mock(self):
         """ News api unmocked test cases """
         r_json = (app.news_api_call())
@@ -32,20 +32,17 @@ class UnmockedTest(unittest.TestCase):
             self.assertFalse(i["url"] == " ", "False or True")
             self.assertFalse(i["published"] == " ", "False or True")
             self.assertFalse(i["content"] == " ", "False or True")
-    
+
     def test_models(self):
         """ Test instances of models from models.py """
         self.assertEqual(self.feedback_test_user.feedback, 'I learned a lot about politics')
         self.assertEqual(self.feedback_test_user.name, 'Jay Amin')
-        
         self.assertEqual(self.quiz_test_question.text, 'Test question for unit test')
         self.assertEqual(self.quiz_test_question.group_name, 'test group')
         self.assertEqual(self.quiz_test_question.multiplier, -32)
-        
         self.assertEqual(self.test_user.email, "test@test.com")
         self.assertEqual(self.test_user.name, "Test McTestface")
         self.assertEqual(self.test_user.img_url, "http://this-is-a-fake-url.com")
-        
         self.assertEqual(self.test_quiz_score.email, "test@test.com")
         self.assertEqual(self.test_quiz_score.score, -99)
 
@@ -54,15 +51,12 @@ class UnmockedTest(unittest.TestCase):
         test_repr = self.feedback_test_user.__repr__()
         correct_repr = "{'name': 'Jay Amin', 'feedback': 'I learned a lot about politics'}"
         self.assertEqual(test_repr, correct_repr)
-        
         test_repr = self.quiz_test_question.__repr__()
         correct_repr = "{'text': 'Test question for unit test', 'group name': 'test group', 'multiplier': -32}"
         self.assertEqual(test_repr, correct_repr)
-        
         test_repr = self.test_user.__repr__()
         correct_repr = "{'email': 'test@test.com', 'name': 'Test McTestface', 'img_url': 'http://this-is-a-fake-url.com'}"
         self.assertEqual(test_repr, correct_repr)
-        
         test_repr = self.test_quiz_score.__repr__()
         correct_repr = "{'email': 'test@test.com', 'score': -99}"
         self.assertEqual(test_repr, correct_repr)
